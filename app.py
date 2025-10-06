fc1534796786e964ca6895ef172ccdf696d3c6d8
Raiffil <natalkap2@gmail.com>
2025-10-06 16:07:34 +0200

    [change] Set up nav bar and basic home page

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html", title="Home")
 
-if __name__ == '__main__':
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Contact")
+
+if __name__ == "__main__":
     app.run()
