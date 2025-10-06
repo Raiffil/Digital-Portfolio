dbbe5cd07fcea430d18330ecd35957b21b6f98d6
Raiffil <natalkap2@gmail.com>
2025-10-06 15:25:40 +0200

    [add] HTML templates and folder structure

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
-from CodeFunctions import home
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home_route():
-    return home()
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
